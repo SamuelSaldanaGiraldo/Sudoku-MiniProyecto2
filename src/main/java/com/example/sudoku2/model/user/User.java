f81bf6f4e1d570b14bba087160328ebbacfc5f44
SamuelSaldanaGiraldo <samuelsaldana586@gmail.com>
2025-10-16 01:35:29 -0500

    actualizacioncommit

--- src/main/java/com/example/sudoku2/model/user/User.java
+++ src/main/java/com/example/sudoku2/model/user/User.java
@@ -1,16 +1,40 @@
 package com.example.sudoku2.model.user;
 
+/**
+ * Represents a user of the Sudoku application.
+ * <p>
+ * This class stores basic information about the player,
+ * such as their nickname, and provides getter and setter
+ * methods for accessing and modifying it.
+ */
 public class User {
+
+    /** The user's chosen nickname. */
     private String nickname;
 
+    /**
+     * Constructs a new {@code User} with the specified nickname.
+     *
+     * @param nickname the nickname of the user.
+     */
     public User(String nickname) {
         this.nickname = nickname;
     }
 
+    /**
+     * Returns the user's nickname.
+     *
+     * @return the nickname of the user.
+     */
     public String getNickname() {
         return nickname;
     }
 
+    /**
+     * Updates the user's nickname.
+     *
+     * @param nickname the new nickname to set.
+     */
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
